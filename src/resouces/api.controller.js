62db7b3ad94dfee2f05c5584fec77c33088cb5d3
sho.tanaka <sho.tanaka@aijus.com>
2019-11-04 19:12:18 +0900

    created a post function

--- src/resouces/api.controller.js
+++ src/resouces/api.controller.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const Todo = require('../db/models/index').todo;
+const { todo, sequelize } = require('../db/models/index');
 
 const formatResponseData = (data) => ({ data });
 
@@ -12,7 +12,7 @@ const statusCode = {
 module.exports = {
     getTodos: async (req, res) => {
         try {
-            const todos = await Todo.findAll({
+            const todos = await todo.findAll({
                 order: [
                     ['id', 'ASC']
                 ],
@@ -25,8 +25,28 @@ module.exports = {
             });
         }
     },
-    postTodo: (req, res) => {
-        send(res, statusCode.OK, 'postTodo', false);
+    postTodo: async (req, res, next) => {
+        try {
+            const dataValues = await sequelize.transaction(
+                async transaction => {
+                    const { title, body, completed = false } = req.body;
+                    return await todo.create(
+                        {
+                            title,
+                            body,
+                            completed,
+                        },
+                        { transaction }
+                    );
+                }
+            );
+            //トランザクション処理の終了を明記しなくても良いのか？ataValuesで終了できてるのか？
+            //例 await transaction.commit();
+            res.status(200).json(formatResponseData(dataValues));
+        } catch (err) {
+            res.status(err.statusCode).json({ error: err });
+            next(err);
+        }
     },
     putTodo: (req, res) => {
         send(res, statusCode.OK, 'putTodo', false);
